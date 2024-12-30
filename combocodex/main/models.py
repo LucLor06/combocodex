2a80688f32c2b56086a4741e2556d21bdfffc684
LucLor06 <LukeLorenzBA@gmail.com>
2024-12-30 15:15:23 -0500

    Create AbstractModel model

--- combocodex/main/models.py
+++ combocodex/main/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+
+class AbstractModel(models.Model):
+    name = models.CharField(max_length=32)
+    order = models.PositiveIntegerField(unique=True)
+
+    class Meta:
+        abstract = True
+        orering = ['order']
+
+    def __str__(self):
+        return self.name
