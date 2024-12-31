4ae26eb4106b8e1f463893e8b67ec1c7dc86936a
LucLor06 <LukeLorenzBA@gmail.com>
2024-12-31 10:44:02 -0500

    Add icon method to Weapon and Legend models and add slug field to AbstractModel

--- combocodex/main/models.py
+++ combocodex/main/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from config.settings import STATIC_ROOT
+from django.utils.text import slugify
+
 
 class AbstractModel(models.Model):
     name = models.CharField(max_length=32)
+    slug = models.SlugField()
     order = models.PositiveIntegerField(unique=True)
 
     class Meta:
@@ -10,9 +14,20 @@ class AbstractModel(models.Model):
 
     def __str__(self):
         return self.name
-    
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+
 class Weapon(AbstractModel):
-    ...
+    def icon(self):
+        return f'{STATIC_ROOT}weapons/{self.slug}.png'
+
 
 class Legend(AbstractModel):
     weapons = models.ManyToManyField('Weapon', related_name='legends')
+
+    def icon(self):
+        return f'{STATIC_ROOT}weapons/{self.slug}.png'
