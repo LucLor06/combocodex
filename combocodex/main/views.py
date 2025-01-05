e32e8a3d1fb2cef6e6e2f37198a765c1a274181f
LucLor06 <LukeLorenzBA@gmail.com>
2025-01-05 15:08:45 -0500

    Create daily challenge partial and expand upon home page

--- combocodex/main/views.py
+++ combocodex/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from user.models import User
-from main.models import Combo, WebsiteSocial
+from main.models import Combo, WebsiteSocial, DailyChallenge
 
 def home(request):
-    context = {'socials': WebsiteSocial.objects.all(), 'combo_count': Combo.objects.count(), 'user_count': User.objects.count()}
+    context = {'socials': WebsiteSocial.objects.all(), 'daily_challenge': DailyChallenge.objects.latest('-id'), 'combo_count': Combo.objects.count(), 'user_count': User.objects.count()}
     return render(request, 'home.html', context)
